f395ee6b1ed4f5277ebb8ae8f1a977f00349c01f
toshi43 <shi6to4th@gmail.com>
2022-12-12 13:31:48 +0900

    1212commit2

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -34,6 +34,11 @@ class HomeController extends Controller
         return view('create', compact('tags'));
     }
 
+    public function folders()
+    {
+        return "Hello world";
+    }
+
     public function store(Request $request)
     {
         $posts = $request->all();
